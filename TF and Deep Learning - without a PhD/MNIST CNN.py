c760f986a7c1d7f7301e253882ad066dea36acd8
Ryan Missel <rxm7244@rit.edu>
2018-05-10 14:16:57 -0400

    Fixed Tensor shape issues for CNN, working on dropout methods

--- TF and Deep Learning - without a PhD/MNIST CNN.py
+++ TF and Deep Learning - without a PhD/MNIST CNN.py
@@ -5,8 +5,8 @@ data = input_data.read_data_sets('data/MNIST', one_hot=True)
 images = tf.placeholder(tf.float32, [None, 784])
 true = tf.placeholder(tf.float32, [None, 10])
 
-image_reshape = tf.reshape(images, shape=[None, 28, 28, 1])
-W1 = tf.Variable(tf.truncated_normal([5, 5, 1, 4], stddev=0.1))
+image_reshape = tf.reshape(images, shape=[-1, 28, 28, 1])
+W1 = tf.Variable(tf.truncated_normal([5, 5, 1, 4], stddev=0.1), name='w1')
 B1 = tf.Variable(tf.zeros([4]))
 W2 = tf.Variable(tf.truncated_normal([4, 4, 4, 8], stddev=0.1))
 B2 = tf.Variable(tf.zeros([8]))
@@ -19,9 +19,9 @@ B4 = tf.Variable(tf.zeros([200]))
 W5 = tf.Variable(tf.truncated_normal([200, 10], stddev=0.1))
 B5 = tf.Variable(tf.zeros([10]))
 
-l1 = tf.nn.relu(tf.nn.conv2d(image_reshape, W1, strides=[1,1,1,1], padding='SAME') + B1)
-l2 = tf.nn.relu(tf.nn.conv2d(l1, W2, strides=[1,2,2,1], padding='SAME') + B2)
-l3 = tf.nn.relu(tf.nn.conv2d(l2, W3, strides=[1,2,2,1], padding='SAME') + B3)
+l1 = tf.nn.relu(tf.nn.conv2d(image_reshape, W1, strides=[1, 1, 1, 1], padding='SAME') + B1)
+l2 = tf.nn.relu(tf.nn.conv2d(l1, W2, strides=[1, 2, 2, 1], padding='SAME') + B2)
+l3 = tf.nn.relu(tf.nn.conv2d(l2, W3, strides=[1, 2, 2, 1], padding='SAME') + B3)
 
 reshape = tf.reshape(l3, shape=[-1, 7*7*12])
 l4 = tf.nn.relu(tf.matmul(reshape, W4) + B4)
@@ -37,11 +37,11 @@ acc = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for _ in range(100):
+        for _ in range(1000):
             x_batch, labels = data.train.next_batch(100)
             sess.run(train_step, feed_dict={images: x_batch, true: labels})
 
-            if _ % 1000 == 0:
+            if _ % 100 == 0:
                 accuracy = sess.run(acc, feed_dict={images: x_batch, true: labels})
                 print("Accuracy on iteration", _, ":", accuracy)
 
